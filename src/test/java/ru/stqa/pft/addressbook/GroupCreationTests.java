878d8f4973edb23e84865ba472df440e4cafb243
PaveJI <palbl41@gmail.com>
2020-04-14 20:42:31 +0300

    refactor_addressbook

--- src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -20,30 +20,55 @@ public class GroupCreationTests {
     driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     driver.get("http://localhost/addressbook/group.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("user")).sendKeys(username);
     driver.findElement(By.name("pass")).click();
     driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.name("pass")).sendKeys(password);
     driver.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
+    gotoGroupPage("groups");
+    initCroupCreation("new");
+    fillGroupForm(new GroupData("test22", "test222", "test2222"));
+    submitGroupCreation("submit");
+    returnGroupPage("group page");
+  }
+
+  private void returnGroupPage(String s) {
+    driver.findElement(By.linkText(s)).click();
+  }
+
+  private void submitGroupCreation(String submit) {
+    driver.findElement(By.name(submit)).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test2");
+    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
     driver.findElement(By.name("group_header")).click();
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test22");
+    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     driver.findElement(By.name("group_footer")).click();
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test222");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
+    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+
+  }
+
+  private void initCroupCreation(String s) {
+    driver.findElement(By.name(s)).click();
+  }
+
+  private void gotoGroupPage(String groups) {
+    driver.findElement(By.linkText(groups)).click();
   }
 
   @AfterClass(alwaysRun = true)
