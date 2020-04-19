03d889bfd05d03d4def81175e37b80e57046b214
PaveJI <palbl41@gmail.com>
2020-04-19 19:14:05 +0300

    refactor_addressbook

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,10 +9,16 @@ public class GroupModificationTests extends TestBase{
     @Test
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().selectGroup();
+        int before = app.getGroupHelper().getGroupCount();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test22", "test222", "test2222"));
+        }
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test22", "test222", "test2222"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(before, after);
     }
 }
